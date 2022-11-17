94cedd2b6fe5ca8c5ea8c6d779a446ace8daff69
Gustavo Keller <gustavo.lealtec@gmail.com>
2022-11-16 23:50:56 -0300

    update finaly project

--- src/sender.ts
+++ src/sender.ts
@@ -1,11 +1,29 @@
 import parsePhoneNumber, { isValidPhoneNumber } from "libphonenumber-js"
 import { PhoneNumber } from "libphonenumber-js"
+import { type } from "os"
 import { start } from "repl"
 import { create, Whatsapp, Message, SocketState } from "venom-bot"
 import { toRaw } from "vue"
 
+export type QRcode = {
+    base64Qr: string
+    attempts: number
+}
+
 class Sender {
     private client: Whatsapp
+    private connected: boolean;
+    private qr: QRcode;
+
+
+    get isConnected(): boolean {
+        return this.connected
+    }
+
+    get qrCode(): QRcode {
+        return this.qr
+    }
+
 
     constructor() {
         this.initialize()
@@ -14,7 +32,7 @@ class Sender {
     async sendText(to: string, body: string) {
         // 554891893541@c.us
 
-        if(!isValidPhoneNumber(to, "BR")) {
+        if (!isValidPhoneNumber(to, "BR")) {
             throw new Error("this number is not valid")
         }
 
@@ -22,21 +40,37 @@ class Sender {
             ?.format("E.164")
             ?.replace("+", "") as string
 
-        phoneNumber = phoneNumber.includes("@c.us") 
-            ? phoneNumber 
+        phoneNumber = phoneNumber.includes("@c.us")
+            ? phoneNumber
             : `${phoneNumber}@c.us`
-        
+
+        console.log("phoneNumber", phoneNumber)
+
         await this.client.sendText(phoneNumber, body);
     }
 
     private initialize() {
+        const qr = (
+            base64Qr: string, 
+            asciiQR: string, 
+            attempts: number, 
+            urlCode: string
+        ) => {
+            this.qr = { base64Qr, attempts}
+        }
 
-        const qr = (base64Qrimg: string) => {}
-
-        const status = (statusSession: string) => {}
+        const status = (statusSession: string) => {
+            this.connected = ["isLogged", "qrReadSuccess", "chatsAvailable"].includes(
+                statusSession
+            )
+        }
 
         const start = (client: Whatsapp) => {
             this.client = client
+
+            client.onStateChange((state) => {
+                this.connected = state === SocketState.CONNECTED
+            })
         }
 
         create('api_wa_send', qr, status)
